abb899239414bd20412d1cae4feac5f987d73d47
MrShameer <67554400+MrShameer@users.noreply.github.com>
2024-10-18 02:18:12 +0800

    .

--- main.py
+++ main.py
@@ -1,47 +1,68 @@
 from pyht import Client
 from pydub import AudioSegment
 from pyht.client import TTSOptions
-import os
-import io
-import ollama
-import json
+import os, ollama, json, random, subprocess, shutil
 
-client = Client(
-    user_id=os.getenv("PLAY_HT_USER_ID"),
-    api_key=os.getenv("PLAY_HT_API_KEY"),
-)
+class VideoCreator():
+	def __init__(self) -> None:
+		if os.path.exists("Process"):
+			shutil.rmtree("Process")
+		os.makedirs(f"FinalVideos", exist_ok=True)
+		self.voiceClient = Client(
+			user_id=os.getenv("PLAY_HT_USER_ID"),
+			api_key=os.getenv("PLAY_HT_API_KEY"),
+			auto_connect=False
+		)
+		self.audioPath = ""
+		self.videoFiles = [f for f in os.listdir("RandomVideos") if f.endswith(('.mp4', '.avi', '.mov', '.mkv'))]
 
-audio={
-    "Sarah":"s3://voice-cloning-zero-shot/775ae416-49bb-4fb6-bd45-740f205d20a1/jennifersaad/manifest.json",
-    "John":"s3://voice-cloning-zero-shot/775ae416-49bb-4fb6-bd45-740f205d20a1/jennifersaad/manifest.json",
-}
+		with open('config.json', 'r') as file:
+			self.data = json.load(file)
+		self.data['Prompt'][0]['content'] = self.data['Prompt'][0]['content'].replace('CHARACTER_LIST', str(list(self.data['Characters'].keys())))
 
+	def generateStory(self, story, max):
+		self.data['Prompt'][1]['content'] = story
+		self.data['Prompt'][0]['content'] = self.data['Prompt'][0]['content'].replace('CHARACTER_LIMIT', str(max))
+		response = ollama.chat(model=self.data['Models'][0], messages=self.data['Prompt'])
+		self.script = json.loads(response['message']['content'].replace('```json', '').replace('```', ''))
+		print(self.script)
+		return self.script
 
-response = ollama.chat(model='gemma2', messages=[
-  {
-    'role': 'system',
-    'content': """You are an AI tasked to generate an interesting story based on the prompt from the user and make sure the story is in a form of dialogs only. 
-    Use only 2 characters 'Sarah' and 'John'. The output should be in a json format where it should have a list containing a key called 'Character' with their name as the value and a key called 'Dialog' with their dialog as their value. 
-    Respond with only a JSON output according to the format and no preamble and no any explanation""",
+	def createAudios(self, num, script):
+		os.makedirs(f"Process/{num}/Audios/", exist_ok=True)
+		combinedAudio = AudioSegment.empty()
+		for index, dialog in enumerate(script):
+			options = TTSOptions(voice=self.data['Characters'][dialog["Character"]])
+			audioPath = f"Process/{num}/Audios/{index}-{dialog["Character"]}.mp3"
+			with open(audioPath, "ab") as f:
+				for chunk in self.voiceClient.tts(dialog["Dialog"], options):
+					f.write(chunk)
 
-  }, 
-  {
-    'role': 'user',
-    'content':'write a horror story'
-  }
+			audio_segment = AudioSegment.from_file(audioPath)
+			combinedAudio += audio_segment
+		return combinedAudio.export(f"Process/{num}/finalAudio.mp3", format="mp3")
+	
+	def createVideos(self, num, audioPath):
+		video_path = f"RandomVideos/{random.choice(self.videoFiles)}"
+
+		command = [
+        'ffmpeg','-y','-i', video_path, '-i', audioPath,
+        '-c:v', 'copy', '-c:a', 'aac', '-map', '0:v:0', '-map', '1:a:0',
+        '-shortest', f'FinalVideos/{num}-Video.mp4'
+    	]
+
+		subprocess.run(command, check=True)
+		print("Output", f'FinalVideos/{num}-Video.mp4')
+
+	def create(self, stories):
+		for index, story in enumerate(stories):
+			print(story)
+			audio = self.createAudios(index, self.generateStory(*story))
+			self.createVideos(index, audio.name)
+
+videos = [
+	("Write a story where a long time friend decided to text a friend but has hidden agenda, and make it really engaging make the store in like a text message perspective. Make the story interesting, with triller, and keeps readers hooked up and on their toes. Add suspense if needed.", 2),
+	# ("write a happy story", 5)
 ]
-# stream=True,
-)
-# for chunk in response:
-#   print(chunk['message']['content'], end='', flush=True)
-print(response['message']['content'])
-script=json.loads(response['message']['content'].replace('```json', '').replace('```', '')) 
-print(script)
-
-
-for index, dialog in enumerate(script):
-    print(dialog["Character"],dialog["Dialog"])
-    options = TTSOptions(voice=audio[dialog["Character"]])
-    with open(f"Audios/{dialog["Character"]} {index}.mp3", "wb") as f:
-        for chunk in client.tts(dialog["Dialog"], options):
-            f.write(chunk)
+video = VideoCreator()
+video.create(videos)
